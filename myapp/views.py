9d539438f88a900f24da0e276e07752da950ca51
zhangs <807167807@qq.com>
2019-02-01 18:04:05 +0800

    web

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+def getData(request):
+    dic={'name':'zs','age':30,'like':['food','noodle']}
+    str=json.dumps(dic)
+    return HttpResponse(str)
